cfcc80e06eb0634257495e461046b9cce515d197
Tejas B P <58722292+tejas69-art@users.noreply.github.com>
2024-11-09 20:37:18 +0530

    Delete server directory

--- server/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.post('/requestOTP', authController.requestOTP);
-router.post('/verifyOTP', authController.verifyOTP);
-
-module.exports = router;
